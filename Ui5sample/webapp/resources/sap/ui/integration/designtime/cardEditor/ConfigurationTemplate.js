fad462fb514e5e987515f09f41e8b6475589c211
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-11-16 16:13:58 +0530

    .

---
+++ Ui5sample/webapp/resources/sap/ui/integration/designtime/cardEditor/ConfigurationTemplate.js
@@ -0,0 +1,6 @@
+/*!
+ * OpenUI5
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
+ * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
+ */
+sap.ui.define(["sap/ui/integration/Designtime"],function(D){"use strict";return function(){return new D({form:{items:{}},preview:{modes:"Abstract"}});};});
