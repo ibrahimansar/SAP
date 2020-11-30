cec280ebdd212261ae9e9779ebb961e1bea582b8
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-11-30 13:46:44 +0530

    .

--- Ui5sample/webapp/controller/App.controller.js
+++ Ui5sample/webapp/controller/App.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], function(Controller) {
-    'use strict';
-    return Controller.extend("sap.ui.demo.walkthrough.App",{
-        onShowHello : function () {
-            alert("Hello There!");
-        }
-    });       
-}); 
+], function (Controller) {
+    "use strict"
+        return Controller.extend("sap.ui.demo.walkthrough.App", {
+
+            onInit: function () {
+                this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+            },
+            
+            onOpenDialog: function () {
+                this.getOwnerComponent().openHelloDialog();
+            }
+        });
+    });
